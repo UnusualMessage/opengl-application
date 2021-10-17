ac72d40476f66f7a72f1924a739bc757222dedd2
UnusualMessage <nikita_kot2@mail.ru>
2021-10-17 20:02:00 +0300

    Add Shader

--- OpenGL_Transformation/Base/Shader.cs
+++ OpenGL_Transformation/Base/Shader.cs
@@ -1,6 +1,129 @@
-﻿namespace TransformationApplication.Base
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TransformationApplication.Base
 {
-    public class Shader
+    public class Shader : IDisposable
     {
+        private readonly Dictionary<string, int> _uniformLocations;
+        private readonly int _handle;
+        private bool _disposed;
+
+        public Shader(string vertexShaderPath, string fragmentShaderPath)
+        {
+            string shaderSource = File.ReadAllText(vertexShaderPath);
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, shaderSource);
+            CompileShader(vertexShader);
+
+            shaderSource = File.ReadAllText(fragmentShaderPath);
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, shaderSource);
+            CompileShader(fragmentShader);
+
+            _handle = GL.CreateProgram();
+
+            GL.AttachShader(_handle, vertexShader);
+            GL.AttachShader(_handle, fragmentShader);
+
+            LinkProgram(_handle);
+
+            GL.DetachShader(_handle, vertexShader);
+            GL.DetachShader(_handle, fragmentShader);
+            GL.DeleteShader(fragmentShader);
+            GL.DeleteShader(vertexShader);
+
+            GL.GetProgram(_handle, GetProgramParameterName.ActiveUniforms, out int numberOfUniforms);
+
+            _uniformLocations = new Dictionary<string, int>();
+
+            for (int i = 0; i < numberOfUniforms; i++)
+            {
+                string key = GL.GetActiveUniform(_handle, i, out _, out _);
+                int location = GL.GetUniformLocation(_handle, key);
+                _uniformLocations.Add(key, location);
+            }
+        }
+
+        private static void CompileShader(int shader)
+        {
+            GL.CompileShader(shader);
+
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out int code);
+            if (code != (int)All.True)
+            {
+                string infoLog = GL.GetShaderInfoLog(shader);
+                throw new Exception($"Error occurred while compiling Shader({shader}).\n\n{infoLog}");
+            }
+        }
+
+        private static void LinkProgram(int program)
+        {
+            GL.LinkProgram(program);
+
+            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out int code);
+            if (code != (int)All.True)
+            {
+                throw new Exception($"Error occurred while linking Program({program})");
+            }
+        }
+
+        public void Use()
+        {
+            GL.UseProgram(_handle);
+        }
+
+        public int GetAttribLocation(string attribName)
+        {
+            return GL.GetAttribLocation(_handle, attribName);
+        }
+
+        public void SetInt(string name, int data)
+        {
+            GL.UseProgram(_handle);
+            GL.Uniform1(_uniformLocations[name], data);
+        }
+
+        public void SetFloat(string name, float data)
+        {
+            GL.UseProgram(_handle);
+            GL.Uniform1(_uniformLocations[name], data);
+        }
+
+        public void SetMatrix4(string name, Matrix4 data)
+        {
+            GL.UseProgram(_handle);
+            GL.UniformMatrix4(_uniformLocations[name], true, ref data);
+        }
+
+        public void SetVector3(string name, Vector3 data)
+        {
+            GL.UseProgram(_handle);
+            GL.Uniform3(_uniformLocations[name], data);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                GL.DeleteProgram(_handle);
+
+                _disposed = true;
+            }
+        }
+
+        ~Shader()
+        {
+            GL.DeleteProgram(_handle);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
