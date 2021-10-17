9eeffd8bf50e572ddbb760acb4111e4ac900fd20
UnusualMessage <nikita_kot2@mail.ru>
2021-10-17 20:26:45 +0300

    Realize SceneObject interface and Scene Objects Drawer

--- OpenGL_Transformation/Base/SceneObjectDrawer.cs
+++ OpenGL_Transformation/Base/SceneObjectDrawer.cs
@@ -1,6 +1,29 @@
-﻿namespace TransformationApplication.Base
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+
+using TransformationApplication.SceneObjects.Base;
+
+namespace TransformationApplication.Base
 {
     public class SceneObjectDrawer
     {
+        private readonly SceneObject _sceneObject;
+
+        public SceneObjectDrawer(SceneObject sceneObject)
+        {
+            _sceneObject = sceneObject;
+        }
+
+        public void Draw(Matrix4 modelMatrix, Matrix4 viewMatrix, Matrix4 projectionMatrix)
+        {
+            Shader objectShader = _sceneObject.GetShader();
+            objectShader.Use();
+
+            objectShader.SetMatrix4("model", modelMatrix);
+            objectShader.SetMatrix4("view", viewMatrix);
+            objectShader.SetMatrix4("projection", projectionMatrix);
+
+            GL.DrawArrays(PrimitiveType.Triangles, 0, _sceneObject.GetVertices().Length);
+        }
     }
 }
--- OpenGL_Transformation/SceneObjects/Base/SceneObject.cs
+++ OpenGL_Transformation/SceneObjects/Base/SceneObject.cs
@@ -1,6 +1,15 @@
-﻿namespace TransformationApplication.SceneObjects.Base
+﻿using TransformationApplication.Base;
+
+using System;
+
+namespace TransformationApplication.SceneObjects.Base
 {
-    public abstract class SceneObject
+    public abstract class SceneObject : IDisposable
     {
+        public abstract float[] GetVertices();
+        public abstract Shader GetShader();
+        public abstract int GetLocation(string target);
+
+        public abstract void Dispose();
     }
 }
