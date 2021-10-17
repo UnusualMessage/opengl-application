4b4e3d8cb9b2ffb2553cc53299146efbba99af63
UnusualMessage <nikita_kot2@mail.ru>
2021-10-17 16:07:43 +0300

    Add project files.

---
+++ OpenGL_Transformation/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace TransformationApplication
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
