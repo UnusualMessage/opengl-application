1d9bace5e34678a831afd123c6ed2b154e7816fc
UnusualMessage <nikita_kot2@mail.ru>
2021-10-19 18:56:28 +0300

    Add translation and rotation classes

---
+++ OpenGL_Transformation/SceneObjects/Model/Rotation.cs
@@ -0,0 +1,16 @@
+﻿namespace TransformationApplication.SceneObjects.Model
+{
+    public class Rotation
+    {
+        public float RotationByX { get; set; }
+        public float RotationByY { get; set; }
+        public float RotationByZ { get; set; }
+
+        public Rotation(float x, float y, float z)
+        {
+            RotationByX = x;
+            RotationByY = y;
+            RotationByZ = z;
+        }
+    }
+}
---
+++ OpenGL_Transformation/SceneObjects/Model/Translation.cs
@@ -0,0 +1,16 @@
+﻿namespace TransformationApplication.SceneObjects.Model
+{
+    public class Translation
+    {
+        public float TranslationByX { get; set; }
+        public float TranslationByY { get; set; }
+        public float TranslationByZ { get; set; }
+
+        public Translation(float x, float y, float z)
+        {
+            TranslationByX = x;
+            TranslationByY = y;
+            TranslationByZ = z;
+        }
+    }
+}
