39709d83c2388646344c615a9d9ee4a8b5f521ce
UnusualMessage <nikita_kot2@mail.ru>
2021-11-08 20:59:41 +0300

    Add Frustum class

---
+++ OpenGL_Transformation/SceneObjects/Frustum.cs
@@ -0,0 +1,18 @@
+﻿using TransformationApplication.Base;
+using TransformationApplication.SceneObjects.Base;
+
+using OpenTK.Mathematics;
+using OpenTK.Graphics.OpenGL4;
+
+namespace TransformationApplication.SceneObjects
+{
+    public class Frustum : SceneComponent, IVisible
+    {
+        public Frustum() { }
+
+        public void Draw(Matrix4 model, Matrix4 view, Matrix4 projection)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
